d7c67e584e80c51537602f2ce97bc442014adf06
Unitech6 <Unitech6@Unitech1-PC>
2017-06-30 16:02:39 +0530

    updated by sam...

--- src/com/iberrylogin/dao/VerifyLogin.java
+++ src/com/iberrylogin/dao/VerifyLogin.java
@@ -11,7 +11,28 @@ public class VerifyLogin {
     public static boolean validate(String name, String pass) {
         //This program validates the user id and password
     	boolean status = false;
-        
+    	try{
+    		
+        	
+            Class.forName("com.mysql.jdbc.Driver");
+        	Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/user","root","root");
+        	
+        	PreparedStatement pst=con.prepareStatement("select* from login");
+        	pst.executeQuery();
+        	ResultSet rs=pst.executeQuery();
+        	while(rs.next())
+        	{
+        	if(rs.getString(1).equals(name)&&rs.getString(2).equals(pass))
+        	{
+        		status=true;
+        		return status;
+        	}
+        	}
+        	}
+        	catch(Exception e) 
+        	{
+    	e.printStackTrace();
+        	}
         return status;
     }
 }
--- src/com/iberrylogin/servlets/LoginServlet.java
+++ src/com/iberrylogin/servlets/LoginServlet.java
@@ -1,11 +1,16 @@
 package com.iberrylogin.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.iberrylogin.dao.VerifyLogin;
 
 /**
  * Servlet implementation class LoginServlet
@@ -28,6 +33,25 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+         PrintWriter out=response.getWriter();
+		
+		response.setContentType("text/html");
+		String username=request.getParameter("username");
+		String password=request.getParameter("password");
+		
+		
+		if (VerifyLogin.validate(username,password))
+		{
+			RequestDispatcher rd=request.getRequestDispatcher("welcome.jsp");
+			rd.forward(request,response);
+		}
+		
+		else 
+		{
+			RequestDispatcher rd=request.getRequestDispatcher("error.jsp");
+			rd.forward(request,response);
+			
+		}
 	}
 
 	/**
